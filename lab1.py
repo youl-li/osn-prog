f175638140ef816d9f12e189476b8bd89a82d945
Yana Naumovych <frekday3@gmail.com>
2025-04-07 23:19:25 +0300

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -1,10 +1,20 @@
-text = "Hello world"
-name = "Yana"
-last_name = "Naumovych"
-age = 16
-print(text)
-print(name,last_name)
-print(age)
-print(type(name))
-print(type(last_name))
-print(type(age))
+class Yana:
+    def __init__(self, name="Yana", surname="Naumovich", birth_year=2008):
+        self.name = name
+        self.surname = surname
+        self.birth_year = birth_year
+
+    def calculate_course(self):
+        if self.birth_year is None:
+            return "Рік народження не вказано"
+        current_year = 2025
+        age = current_year - self.birth_year
+        course = age - 17 + 1
+        if course < 1:
+            return "Ще не студент"
+        elif course > 4:
+            return "Закінчив бакалаврат"
+        return f"{course} курс"
+
+    def full_name_list(self):
+        return [self.name, self.surname]
